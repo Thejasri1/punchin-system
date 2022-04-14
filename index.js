8de1a029aec2fd32e14693cb32b0ed323e98bf3d
Thejasri <tjs9@Thejasris-MacBook-Air.local>
2022-04-14 16:57:21 +0530

    files modified

--- index.js
+++ index.js
@@ -5,22 +5,40 @@ const ejs = require("ejs");
 const pug = require("pug");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
-const connectDB = require("./config/db");
 const mongoose = require("mongoose");
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
-dotenv.config({ path: "./config/config.env" });
-connectDB();
-app.use(bodyParser.json());
+//local database connectio url
+const url = "mongodb://localhost:27017/TJS";
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//pug connection view engine
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
-app.get("/api/punch", (_, res) => {
+app.get("/", (_, res) => {
   res.render("punchIn", {
     title: "PunchIn/Out-system",
+  });
+});
+
+app.post("/punch", (req, res) => {
+  //id of the user
+  const formData = {
+    id: req.body.id,
+  };
+  console.log(req.body);
+  res.render("punchIn", {
+    title: "PunchIn/Out-system",
+  });
+  mongoose.connect(url, function (_, db) {
+    db.collection("punching").insertOne(formData, function (_, result) {
+      console.log("id has been inserted");
+      db.close;
+    });
   });
 });
 
