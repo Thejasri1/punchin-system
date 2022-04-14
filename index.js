e03a6975d47ca3c1ae3adabbb71c64c10f6fc8a7
Thejasri <tjs9@Thejasris-MacBook-Air.local>
2022-04-14 15:24:35 +0530

    pug file added data modified initial commit files modified latest commit punIn/Out commit first commit added env capability

--- index.js
+++ index.js
@@ -1,40 +1,155 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const ejs = require("ejs");
-
+const pug = require("pug");
+const bodyParser = require("body-parser");
+const requestIp = require("request-ip");
+const mongoose = require("mongoose");
+const { WebClient } = require("@slack/web-api");
+const useragent = require("express-useragent");
+const axios = require("axios").default;
+const dotenv = require("dotenv");
 const hostname = "127.0.0.1";
 const port = 3000;
 
-// Require static assets from public folder
-app.use(express.static(path.join(__dirname, "public")));
-// Set view engine as EJS
-app.engine("ejs", require("ejs").renderFile);
-app.set("view engine", "ejs");
-// Set 'views' directory for any views
-// being rendered res.render()
-// app.set("views", path.join(__dirname, ""));
+dotenv.config();
+
+//local database connection url
+const url = "mongodb://localhost:27017/TJS";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//pug connection view engine
+app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
+app.use(useragent.express());
 
-// app.get("/form", express.static(__dirname + "/index.html"));
+//punch in landing page method:
+app.get("/", (_, res) => {
+  return res.render("punchIn", {
+    title: "PunchIn/Out-system",
+  });
+});
+//List of employess :
+const employees = {
+  "000": "Priyank",
+  "001": "Chetna",
+  "002": "Vinay",
+  "003": "Sridhar",
+  "004": "Nikunj",
+  "005": "Jayanth",
+  "006": "Avantika",
+  "007": "Harsh",
+  "008": "Omprakash",
+  "009": "Thejasri",
+};
 
-// let publicPath = path.join(__dirname, "public");
+//punch in submit method:
+app.post("/", async (req, res) => {
+  //if data is empty it will not store the data into db instead it will redirect to the home page
+  if (req.body.id === "") return res.redirect("/");
 
-app.get("/student", (_, res) => {
-  // res.sendFile(`${publicPath}/home.html`);
-  const student = [
-    {
-      name: "teja",
-      age: 23,
-      contact: 7463882344,
+  //IP Address :
+  const clientIp = requestIp.getClientIp(req);
+  //userAgent :
+  const userAgent = JSON.stringify(req.useragent);
+
+  //current time :
+  let current_time_obj = new Date().toLocaleTimeString();
+  //Form Data:
+  const formData = {
+    id: req.body.id,
+    [new Date().toLocaleDateString()]: {
+      current_date_obj: [{ "Punch In": current_time_obj }],
     },
-    { name: "teja", age: 23, contact: 7463882344 },
-  ];
-  // res.sendFile(path.join(__dirname, "views/index.html"));
-  res.render("student", { student: student });
-  // res.render("student", { student });
+  };
+  let latestEntry = undefined;
+  const api = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${req.body.lat},${req.body.long}&key=${process.env.GOOGLE_API_KEY}`;
+  let results;
+  const getAddressByReverseCoding = async () => {
+    try {
+      const response = await axios.get(api);
+      results = JSON.stringify(response.data.results[0].formatted_address);
+    } catch (error) {
+      console.error(error.response.data);
+      return res.redirect("/?e=error");
+    }
+  };
+  await getAddressByReverseCoding();
+
+  //Database connection:
+  mongoose.connect(url, async (_, db) => {
+    const databaseConnection = db.collection("punching");
+    const findResult = await databaseConnection.findOne({ id: req.body.id });
+    //find results :
+    if (findResult) {
+      //Update data for existed Id for only current date:
+      if (findResult[new Date().toLocaleDateString()]) {
+        latestEntry = Object.keys(
+          findResult[new Date().toLocaleDateString()]["current_date_obj"][
+            findResult[new Date().toLocaleDateString()]["current_date_obj"]
+              .length - 1
+          ]
+        )[0];
+        databaseConnection.updateOne(
+          { _id: findResult._id },
+          {
+            $push: {
+              [`${new Date().toLocaleDateString()}.current_date_obj`]: {
+                [latestEntry === "Punch In" ? "Punch Out" : "Punch In"]:
+                  current_time_obj,
+              },
+            },
+          }
+        );
+      } else {
+        //undates data nestedly for previously stored data:
+        databaseConnection.updateOne(
+          { _id: findResult._id },
+          {
+            $push: {
+              [`${new Date().toLocaleDateString()}.current_date_obj`]: {
+                [latestEntry === "Punch In" ? "Punch Out" : "Punch In"]:
+                  current_time_obj,
+              },
+            },
+          }
+        );
+      }
+    } else {
+      // insert the data id is as new entry:
+      databaseConnection.insertOne(formData, () => {
+        db.close;
+      });
+    }
+
+    // An access token (from your Slack app or custom integration - xoxp, xoxb)
+    const token = process.env.SLACK_API_KEY;
+
+    const web = new WebClient(token);
+
+    // This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
+    const conversationId = process.env.CONVERSATION_ID;
+
+    try {
+      await web.chat.postMessage({
+        channel: conversationId,
+        text: `User : ${employees[req.body.id] || "Unknown"} \n ${
+          latestEntry === "Punch In" ? "Punch Out" : "Punch In"
+        } : ${current_time_obj} \n Location: ${results},${req.body.lat},${
+          req.body.long
+        }\n IP Address: ${clientIp} \n User Agent: ${userAgent}`,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+
+    //after executing the all the process it will redirect it home page :
+    return res.redirect("/?e=success");
+  });
 });
 
+//server is running at this port:
 app.listen(port, hostname, () => {
-  console.log(`server is running at http://${hostname}:${port}`);
+  return console.log(`server is running at http://${hostname}:${port}`);
 });
---
+++ views/punchIn.js
@@ -0,0 +1,89 @@
+window.addEventListener("load", function () {
+  const button = document.getElementById("formId");
+  button.textContent =
+    localStorage.getItem("punchState") === "Punch In"
+      ? "Punch Out"
+      : "Punch In";
+  document.getElementById("inputId").value = localStorage.getItem("id");
+  const form = document.getElementById("formSubmit");
+  form.onsubmit = function (e) {
+    e.preventDefault();
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        localStorage.setItem("punchState", button.textContent);
+        localStorage.setItem("id", document.getElementById("inputId").value);
+        document.getElementById("lat").value = position.coords.latitude;
+        document.getElementById("long").value = position.coords.longitude;
+        form.submit();
+      }, showError);
+    } else {
+      console.log("Browser does not support geolocation");
+    }
+  };
+});
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else {
+    console.log("Browser does not support geolocation");
+  }
+}
+function showPosition(position) {
+  localStorage.setItem("lat", position.coords.latitude);
+  localStorage.setItem("long", position.coords.longitude);
+  document.getElementById("lat").value = position.coords.latitude;
+  document.getElementById("long").value = position.coords.longitude;
+}
+function showError(error) {
+  if (error.PERMISSION_DENIED) {
+    console.log(" The user have denied the request for geolocation.");
+  }
+}
+getLocation();
+const sendNotifcation = () => {
+  new Notify({
+    status: "success",
+    title: "“Successfully Recorded”",
+    effect: "fade",
+    speed: 200,
+    customClass: "",
+    customIcon: "",
+    showIcon: true,
+    showCloseButton: true,
+    autoclose: true,
+    autotimeout: 3000,
+    gap: 0,
+    distance: 0,
+    type: 1,
+    position: "right top",
+  });
+};
+
+const sendErrorNotifcation = () => {
+  new Notify({
+    status: "error",
+    title: "“Unable to Record”",
+    effect: "fade",
+    speed: 200,
+    customClass: "",
+    customIcon: "",
+    showIcon: true,
+    showCloseButton: true,
+    autoclose: true,
+    autotimeout: 3000,
+    gap: 0,
+    distance: 0,
+    type: 1,
+    position: "right top",
+  });
+};
+
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+if (params.e === "success") {
+  sendNotifcation();
+  window.history.pushState({}, "", "/");
+} else if (params.e === "error") {
+  sendErrorNotifcation();
+  window.history.pushState({}, "", "/");
+}
